3202dbf547ae7c978f1d02d710cacebcf946bc2c
SabirovAsadbek8802 <sabirovasadbek932848802@gmail.com>
2024-04-07 14:32:57 +0500

    test1

--- main/forms.py
+++ main/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 
 class ModalForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'modal__input'}), template_name='main/index.html')
-    phone_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'modal__input'}), template_name='main/index.html')  # Assuming phone number is a string
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'modal__input'}))
+    phone_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'modal__input'}))  # Assuming phone number is a string
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'req_input email'}) ,template_name='main/request.html')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'req_input email'}))
     phone_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'req_input tel'}))  # Assuming phone number is a string
-    fullname = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
-    pick_up = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
-    delivery = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
-    vehicle_type = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
-    transport_type = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
-    car_info = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}), template_name='main/request.html')
+    fullname = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
+    pick_up = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
+    delivery = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
+    vehicle_type = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
+    transport_type = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
+    car_info = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'req_input'}))
 
 
 class FooterForm(forms.Form):
